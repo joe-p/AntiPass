4a9b896f43d78da298a651a77f83cae51963eae1
Joe Polny <joepolny@gmail.com>
2021-11-23 19:45:59 -0500

    add default length value

--- src/antipass.js
+++ src/antipass.js
@@ -11,7 +11,7 @@ export function initialize() {
 
     const username = (new URL(document.location)).searchParams.get('username') || ''
     const service = (new URL(document.location)).searchParams.get('service') || ''
-    const length = (new URL(document.location)).searchParams.get('length') || ''
+    const length = (new URL(document.location)).searchParams.get('length') || 12
     const type = (new URL(document.location)).searchParams.get('type') || ''
     const iteration = (new URL(document.location)).searchParams.get('iteration') || 1
 
